ac2a9708fbafa2d0797b0d85322b08f68c302fe4
Andreyshanorm <gorbachev_71@bk.ru>
2024-10-05 13:47:26 +0400

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/framawork-team-test-task/',
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
   server: {
     open: true,
   },
